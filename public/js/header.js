6696b69a435e3a14a93e409c8136941ab3d43f6e
Christian Lowe <christglowe@gmail.com>
2018-04-25 23:03:26 -0500

    refactored some js code, added node_modules to gitignore

---
+++ public/js/header.js
@@ -0,0 +1,13 @@
+let $header = $("header");
+let $heroImage = $(".hero-image");
+
+let heightToCover = Number($heroImage.css("height").replace("px", ""));
+
+let $main = $("main");
+
+$(window).scroll(function(){
+  $header.css("top", Math.max(0, heightToCover- $(this).scrollTop() ) );
+}); // Solution to page banner scrolling issue: http://jsfiddle.net/b43hj/
+
+let startingDistance = $header.css("height");
+$main.css("margin-top", startingDistance)
--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,3 @@
-let $header = $("header");
-let $heroImage = $(".hero-image");
-
-let heightToCover = Number($heroImage.css("height").replace("px", ""));
-
-let $main = $("main");
-
-$(window).scroll(function(){
-  $header.css("top", Math.max(0, heightToCover- $(this).scrollTop()));
-}); // Solution to page banner scrolling issue: http://jsfiddle.net/b43hj/
-
-let startingDistance = $header.css("height");
-$main.css("margin-top", startingDistance)
-
 let $hamburger = $(".hamburger");
 let $cross = $(".cross");
 let $pageMenu = $(".pageMenu");
